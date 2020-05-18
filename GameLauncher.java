ccce43eb6a157a48e844361a9567b83be5c4d310
Hiugo-beep <55299837+Hiugo-beep@users.noreply.github.com>
2020-05-18 08:29:35 +0300

    Create GameLauncher.java

---
+++ GameLauncher.java
@@ -0,0 +1,16 @@
+package Interface;
+
+import javax.swing.SwingUtilities;
+
+public class GameLauncher {
+	public static void main(String[] args) {		
+		//фрейм создается и работает в потоке обработки событий
+		
+		
+
+				Interface gui = new Interface();
+
+				//Interface gui1 = new Interface();
+
+	}
+}
